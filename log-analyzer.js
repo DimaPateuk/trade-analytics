a285fa2a78417dd0eda902611411474b112ac184
Dzmitry Patseyuk <dima_pateuk@mail.ru>
2018-03-24 09:38:20 +0300

    continue play the game

--- log-analyzer.js
+++ log-analyzer.js
@@ -9,7 +9,7 @@ function processFile(inputFile, onLine, onClose) {
 
     rl.on('close', onClose);
 }
-processFile('log3.txt', analize, printResult);
+processFile('log2.txt', analize, printResult);
 
 let win = 0;
 let lose = 0;
@@ -22,7 +22,7 @@ let info = {};
 let R = 0;
 function analize (line) {
   if (line.indexOf('Прогноз не оправдался') !== -1) {
-    console.log(R);
+    // console.log(R);
     R += parseInt(line.split('|')[6]) * -1;
     lose++;
     l++;
@@ -37,14 +37,15 @@ function analize (line) {
   if (line.indexOf('Прогноз оправдался') !== -1) {
     win++;
       R += parseInt(line.split('|')[6]) * 1.8;
-    l && (info[l] = info[l] ? info[l] + 1 : 1);
+    info[l] = info[l] ? info[l] + 1 : 1;
     l = 0;
-    console.log(R);
+    // console.log(R);
   }
 
 }
 const minBet = 1;
-var prices = {
+var pricesA = {
+  0: minBet * 1.8,
   1: 2*minBet * 1.8 - minBet,
   2: 6*minBet * 1.8 - 3*minBet,
   3: 1*minBet * 1.8 - 9*minBet,
@@ -57,19 +58,38 @@ var prices = {
   10: 2*minBet * 1.8 - 28*minBet,
 }
 
+var pricesB = {
+  0: minBet * 1.8,
+  1: 2*minBet * 1.8 - minBet,
+  2: 6*minBet * 1.8 - 3*minBet,
+  3: 1*minBet * 1.8 - 9*minBet,
+  4: 1*minBet * 1.8 - 10*minBet,
+  5: 1*minBet * 1.8 - 11*minBet,
+  6: 1*minBet * 1.8 - 12*minBet,
+  7: 1*minBet * 1.8 - 13*minBet,
+  8: 1*minBet * 1.8 - 14*minBet,
+  9: 1*minBet * 1.8 - 15*minBet,
+  10: 1*minBet * 1.8 - 16*minBet,
+}
+
 // console.log(prices);
 
 function printResult () {
   // console.log(win, lose, lresult, moreThenTreeFail, info);
-  // console.log(info);
+  console.log(info);
 
+  printPrices(pricesA);
+  printPrices(pricesB);
+
+    // console.log(R);
+}
+function printPrices(prices) {
   const result = Object.keys(info)
     .map(v => parseInt(v))
     .reduce((res, val) => {
-      console.log(info[val], prices[val]);
+      // console.log(info[val], prices[val]);
       if (!prices[val]) return res;
       return res + info[val] * prices[val];
     }, 0);
     console.log(result);
-    console.log(R);
 }
